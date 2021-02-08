15550a8b7e06bebf09b3a1e4d799dfbfdb3c0f29
Dennis Eichhorn <spl1nes.com@googlemail.com>
2021-02-08 19:10:15 +0100

    test language/export/import which changed a lot of lang files

---
+++ Theme/Timeterminal/Lang/de.lang.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Orange Management
+ *
+ * PHP Version 8.0
+ *
+ * @copyright Dennis Eichhorn
+ * @license   OMS License 1.0
+ * @version   1.0.0
+ * @link      https://orange-management.org
+ */
+declare(strict_types=1);
+
+return ['HumanResourceTimeRecording' => [
+    'Break' => '',
+    'CS0' => '',
+    'CS1' => '',
+    'CS2' => '',
+    'CS3' => '',
+    'CT0' => '',
+    'CT1' => '',
+    'CT2' => '',
+    'CT3' => '',
+    'CT4' => '',
+    'CT5' => '',
+    'D0' => '',
+    'D1' => '',
+    'D2' => '',
+    'D3' => '',
+    'D4' => '',
+    'D5' => '',
+    'D6' => '',
+    'Date' => '',
+    'End' => '',
+    'Recordings' => '',
+    'Start' => '',
+    'Status' => '',
+    'Total' => '',
+    'Type' => '',
+]];
--- Theme/Timeterminal/Lang/en.lang.php
+++ Theme/Timeterminal/Lang/en.lang.php
@@ -4,7 +4,6 @@
  *
  * PHP Version 8.0
  *
- * @package   Modules\HumanResourceTimeRecording
  * @copyright Dennis Eichhorn
  * @license   OMS License 1.0
  * @version   1.0.0
@@ -13,29 +12,29 @@
 declare(strict_types=1);
 
 return ['HumanResourceTimeRecording' => [
-    'Break'      => 'Break',
-    'CS0'        => 'Start',
-    'CS1'        => 'Pause',
-    'CS2'        => 'Continue',
-    'CS3'        => 'End',
-    'CT0'        => 'Office',
-    'CT1'        => 'Remote',
-    'CT2'        => 'Home',
-    'CT3'        => 'Vacation',
-    'CT4'        => 'Sick',
-    'CT5'        => 'On the move',
-    'D0'         => 'Sunday',
-    'D1'         => 'Monday',
-    'D2'         => 'Tuesday',
-    'D3'         => 'Wednesday',
-    'D4'         => 'Thursday',
-    'D5'         => 'Friday',
-    'D6'         => 'Saturday',
-    'Date'       => 'Date',
-    'End'        => 'End',
+    'Break' => 'Break',
+    'CS0' => 'Start',
+    'CS1' => 'Pause',
+    'CS2' => 'Continue',
+    'CS3' => 'End',
+    'CT0' => 'Office',
+    'CT1' => 'Remote',
+    'CT2' => 'Home',
+    'CT3' => 'Vacation',
+    'CT4' => 'Sick',
+    'CT5' => 'On the move',
+    'D0' => 'Sunday',
+    'D1' => 'Monday',
+    'D2' => 'Tuesday',
+    'D3' => 'Wednesday',
+    'D4' => 'Thursday',
+    'D5' => 'Friday',
+    'D6' => 'Saturday',
+    'Date' => 'Date',
+    'End' => 'End',
     'Recordings' => 'Recordings',
-    'Start'      => 'Start',
-    'Status'     => 'Status',
-    'Total'      => 'Total',
-    'Type'       => 'Type',
+    'Start' => 'Start',
+    'Status' => 'Status',
+    'Total' => 'Total',
+    'Type' => 'Type',
 ]];

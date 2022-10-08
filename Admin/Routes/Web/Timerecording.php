c281a57ab1749487cadaf0a54d5e4b03bfa1759e
Dennis Eichhorn <spl1nes.com@googlemail.com>
2022-10-08 21:34:59 +0200

    fix tests

--- Admin/Routes/Web/Timerecording.php
+++ Admin/Routes/Web/Timerecording.php
@@ -1,4 +1,5 @@
-<?php declare(strict_types=1);
+<?php
+declare(strict_types=1);
 
 use Modules\HumanResourceTimeRecording\Controller\TimerecordingController;
 use Modules\HumanResourceTimeRecording\Models\PermissionCategory;

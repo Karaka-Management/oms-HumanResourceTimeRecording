ae176b9a9fc92a7a8848d040552f34eafffd214b
Dennis Eichhorn <spl1nes.com@googlemail.com>
2022-12-16 21:59:21 +0100

    fix url

--- Models/SessionElement.php
+++ Models/SessionElement.php
@@ -8,7 +8,7 @@
  * @copyright Dennis Eichhorn
  * @license   OMS License 1.0
  * @version   1.0.0
- * @link      https://karaka.app
+ * @link      https://jingga.app
  */
 declare(strict_types=1);
 
@@ -19,7 +19,7 @@ namespace Modules\HumanResourceTimeRecording\Models;
  *
  * @package Modules\HumanResourceTimeRecording\Models
  * @license OMS License 1.0
- * @link    https://karaka.app
+ * @link    https://jingga.app
  * @since   1.0.0
  */
 class SessionElement implements \JsonSerializable
--- Models/SessionMapper.php
+++ Models/SessionMapper.php
@@ -8,7 +8,7 @@
  * @copyright Dennis Eichhorn
  * @license   OMS License 1.0
  * @version   1.0.0
- * @link      https://karaka.app
+ * @link      https://jingga.app
  */
 declare(strict_types=1);
 
@@ -24,7 +24,7 @@ use phpOMS\Stdlib\Base\SmartDateTime;
  *
  * @package Modules\HumanResourceTimeRecording\Models
  * @license OMS License 1.0
- * @link    https://karaka.app
+ * @link    https://jingga.app
  * @since   1.0.0
  */
 final class SessionMapper extends DataMapperFactory

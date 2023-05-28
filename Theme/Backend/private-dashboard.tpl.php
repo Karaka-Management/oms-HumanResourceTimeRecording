1f4b0098562467df5288dc06d0fd5551821ffe72
Dennis Eichhorn <spl1nnes.com@googlemail.com>
2023-05-28 01:13:22 +0000

    style fixes, bug fixes

--- Theme/Backend/private-dashboard.tpl.php
+++ Theme/Backend/private-dashboard.tpl.php
@@ -112,7 +112,7 @@ echo $this->getData('nav')->render(); ?>
 <div class="row">
     <div class="col-xs-12">
         <section class="portlet">
-            <div class="portlet-head"><?= $this->getHtml('Recordings'); ?><i class="fa fa-download floatRight download btn"></i></div>
+            <div class="portlet-head"><?= $this->getHtml('Recordings'); ?><i class="lni lni-download download btn end-xs"></i></div>
             <table id="recordingList" class="default">
                 <thead>
                 <tr>

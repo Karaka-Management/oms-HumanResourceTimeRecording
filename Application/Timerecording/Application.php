4d12dd3c641600c1338e1dc15baeabd917b4048a
Dennis Eichhorn <spl1nnes.com@googlemail.com>
2023-06-13 18:55:51 +0000

    Quick backup before crash

--- Application/Timerecording/Application.php
+++ Application/Timerecording/Application.php
@@ -149,7 +149,7 @@ final class Application
 
         /* CSRF token OK? */
         if ($request->hasData('CSRF')
-            && !\hash_equals($this->app->sessionManager->get('CSRF'), $request->getDataString('CSRF'))
+            && !\hash_equals($this->app->sessionManager->data['CSRF'] ?? '', $request->getDataString('CSRF'))
         ) {
             $response->header->status = RequestStatusCode::R_403;
 
@@ -177,13 +177,13 @@ final class Application
 
         if ($account->id > 0) {
             $response->header->l11n = $account->l11n;
-        } elseif ($this->app->sessionManager->get('language') !== null
-            && $response->header->l11n->language !== $this->app->sessionManager->get('language')
+        } elseif (isset($this->app->sessionManager->data['language'])
+            && $response->header->l11n->language !== $this->app->sessionManager->data['language']
         ) {
             $response->header->l11n
                 ->loadFromLanguage(
-                    $this->app->sessionManager->get('language'),
-                    $this->app->sessionManager->get('country') ?? '*'
+                    $this->app->sessionManager->data['language'],
+                    $this->app->sessionManager->data['country'] ?? '*'
                 );
         } else {
             $this->app->setResponseLanguage($request, $response, $this->config);

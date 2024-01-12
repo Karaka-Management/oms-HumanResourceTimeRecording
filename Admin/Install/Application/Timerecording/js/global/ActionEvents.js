198864fb75df264376a129c33c6ecd1894d699bf
Dennis Eichhorn <spl1nnes.com@googlemail.com>
2024-01-12 00:30:21 +0000

    continue implementations

--- Admin/Install/Application/Timerecording/js/global/ActionEvents.js
+++ Admin/Install/Application/Timerecording/js/global/ActionEvents.js
@@ -1,4 +1,4 @@
-import { redirectMessage } from '../../../../jsOMS/Model/Message/Redirect.js';
+import { redirectMessage } from '../../../../jsOMS/Model/Action/Dom/Redirect.js';
 
 export const ACTION_EVENTS = {
     'redirect': redirectMessage, /** global: redirectMessage */
